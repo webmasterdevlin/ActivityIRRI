4b239ace9a413b33e8368fd4d9106c3f10b32d80
Devlin Duldulao <webmasterdevlin@gmail.com>
2019-05-17 15:45:27 +0800

    created UserService with signin method

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserModel } from '../models/user.model';
+import { BaseUrl } from './api.config';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  signin(userModel: UserModel): Observable<any> {
+    return this.http.post<UserModel>(BaseUrl.authentication, userModel, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+}
