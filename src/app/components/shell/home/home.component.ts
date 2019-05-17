cec6e9c81c2f6f395d0302e7a877e45d0469f541
Devlin Duldulao <webmasterdevlin@gmail.com>
2019-05-17 14:11:25 +0800

    created removeItem functionality

--- src/app/components/shell/home/home.component.ts
+++ src/app/components/shell/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentModel } from 'src/app/models/department.model';
 import { DepartmentService } from 'src/app/services/department.service';
-
+import { catchError } from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,5 +17,31 @@ export class HomeComponent implements OnInit {
       this.departments = response.data;
       console.log(response.data);
     });
+  }
+
+  removeItem(_id: string, name: string): void {
+    const confirmed = confirm(`Delete ${name}?`);
+
+    if (confirmed) {
+      this.removeFromDatabase(_id);
+    }
+  }
+
+  private removeFromDatabase(_id: string): void {
+    const index = this.departments.findIndex(d => d._id === _id);
+    const departmentToRemove = this.departments.find(d => d._id === _id);
+
+    // Optimistic update
+    this.departments.splice(index, 1);
+
+    this.departmentService
+      .deleteDepartment(_id)
+      .pipe(
+        catchError(() =>
+          // Rollback
+          this.departments.splice(index, 0, departmentToRemove)
+        )
+      )
+      .subscribe();
   }
 }
